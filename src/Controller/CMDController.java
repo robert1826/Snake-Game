c6cbc5a67a27a1b280e77b05c25996959c24d79b
robert <robert@samsung>
2017-01-29 23:36:22 +0200

    initial controller for CMD

---
+++ src/Controller/CMDController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import Model.Snake;
+import View.CommandLineView;
+
+public class CMDController {
+	Snake snake;
+	CommandLineView view;
+	
+	public CMDController() {
+		
+	}
+}

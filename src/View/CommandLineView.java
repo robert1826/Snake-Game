37f932872b4b7435f3634ffa3de8b577389a5c62
robert <robert1826@gmail.com>
2017-01-31 23:17:58 +0200

    updated the view and model

--- src/View/CommandLineView.java
+++ src/View/CommandLineView.java
@@ -1,37 +1,24 @@
 package View;
 
-import java.awt.Point;
+import Model.Grid;
 
-import Model.Snake;
-
-public class CommandLineView {
-	static final int EMPTY_VIEW_CODE = 0;
-	static final int SNAKE_VIEW_CODE = 1;
-	static final int MOUSE_VIEW_CODE = 2;
+public class CommandLineView {		
+	private Grid grid;
 	
-	private int width, height;
-	private Snake snake;
-	
-	public CommandLineView(int w, int h, Snake s) {
-		this.width = w;
-		this.height = h;
-		this.snake = s;
+	public CommandLineView(Grid g) {
+		this.grid = g;
 	}
 	
 	public void updateView(){
-		int[][] grid = new int[width][height];
-		Point[] snakeBody = snake.getBody();
-		
-		for(Point p : snakeBody)
-			grid[p.x][p.y] = SNAKE_VIEW_CODE;
+		int[][] g = grid.getGrid();
 			
-		for(int i = 0; i < width; i++){
-			for(int j = 0; j < height; j++){
-				switch (grid[i][j]) {
-				case SNAKE_VIEW_CODE:
+		for(int j = 0; j < grid.getHeight(); j++){
+			for(int i = 0; i < grid.getWidth(); i++){
+				switch (g[i][j]) {
+				case Grid.SNAKE_VIEW_CODE:
 					System.out.print("#");
 					break;
-				case MOUSE_VIEW_CODE:
+				case Grid.MOUSE_VIEW_CODE:
 					System.out.print("^");
 					break;
 				default:

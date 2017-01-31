f40417578a34f897f2548a50162dde34c1cf81aa
robert <robert1826@gmail.com>
2017-01-31 23:21:51 +0200

    updated grid

--- src/Model/Grid.java
+++ src/Model/Grid.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.awt.Point;
+import java.util.Random;
 
 public class Grid {
 	public static final int EMPTY_VIEW_CODE = 0;
@@ -10,11 +11,13 @@ public class Grid {
 	private Point mousePos;
 	private Snake snake;
 	private int width, height;
+	private Random random;
 	
 	public Grid(Snake s, int w, int h){
 		snake = s;
 		this.width = w;
 		this.height = h;
+		random = new Random();
 	}
 
 	public int getWidth() {
@@ -25,13 +28,23 @@ public class Grid {
 		return height;
 	}
 	
+	Point getRandomMousePos() {
+		int x = random.nextInt(getWidth() + 1);
+		int y = random.nextInt(getHeight() + 1);
+		return new Point(x, y);
+	}
+	
 	public int[][] getGrid(){
 		int[][] grid = new int[width][height];
 		Point[] snakeBody = snake.getBody();
 		
 		for(Point p : snakeBody)
 			grid[p.x][p.y] = SNAKE_VIEW_CODE;
-			
+		
+		if(mousePos == null)
+			mousePos = getRandomMousePos();
+		grid[mousePos.x][mousePos.y] = MOUSE_VIEW_CODE;
+		
 		return grid;
 	}
 }

dcf4b9eb556c1b22916509e853a5c89a6c6481bb
robert <robert1826@gmail.com>
2017-01-31 22:33:18 +0200

    added Mouse class

--- src/Model/Mouse.java
+++ src/Model/Mouse.java
@@ -1,5 +1,15 @@
 package Model;
 
-public class Mouse {
+import java.awt.Point;
 
+public class Mouse {
+	private Point pos;
+	
+	public Mouse(int x, int y){
+		pos = new Point(x, y);
+	}
+	
+	public Point getPos(){
+		return pos.getLocation();
+	}
 }

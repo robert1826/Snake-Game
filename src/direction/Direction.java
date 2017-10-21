653791fdc31e6c09dfcf186498acad872d09da25
robert1826 <robert1826@gmail.com>
2017-10-21 21:31:04 +0200

    added axis documentation

--- src/direction/Direction.java
+++ src/direction/Direction.java
@@ -1,6 +1,14 @@
 package direction;
 
 public abstract class Direction {
+//	the axis are like this : 
+//	-----------------> y
+//	|
+//	|
+//	|
+//	|
+//
+//	x
 	
 	public abstract int getDx();
 	public abstract int getDy();
